e1166f2bfb23b80daa14e8d5e6791f82a6a315aa
Haro231 <1419172@rgu.ac.uk>
2016-04-13 01:09:33 +0100

    do it

---
+++ idx2.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Haro231
+ * Date: 13/04/2016
+ * Time: 01:08
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Information</title>
+    <link type="text/css" rel="stylesheet" href="stylesheet2.css"/>
+</head>
+<body>
+
+<?php
+$name = $_POST['firstname'];
+ $last = $_POST['lastname'];
+ $gender = $_POST['gender'];
+ //$origin = $_POST['dropdown'];
+ $interest = $_POST['interest'];
+ $description = $_POST['description'];
+ $pic = $_POST['fileupload'];
+
+
+
+
+ echo "<h2>Your Input: </h2>";
+echo "FirstName:  " .$name;
+echo "<br>";
+echo "LastName:  " .$last;
+echo "<br>";
+echo "Gender:   " .$gender;
+echo "<br>";
+echo "Interest:   " .$interest;
+echo "<br>";
+echo "Description:   " .$description;
+
+?>
+
+</body>
+</html>
