828db79681f85c642da98e16694b6e51a81c66a1
Haro231 <1419172@rgu.ac.uk>
2016-04-13 01:21:27 +0100

    do it

--- idx2.php
+++ idx2.php
@@ -7,4 +7,28 @@
  */
 ?>
 
+<?php
+$name = $_POST['firstname'];
+$last = $_POST['lastname'];
+$gender = $_POST['gender'];
+//$origin = $_POST['dropdown'];
+$interest = $_POST['interest'];
+$description = $_POST['description'];
+//$pic = $_POST['fileupload'];
 
+
+
+
+echo "<h2>Your Input: </h2>";
+echo "FirstName:  " .$name;
+echo "<br>";
+echo "LastName:  " .$last;
+echo "<br>";
+echo "Gender:   " .$gender;
+echo "<br>";
+echo "Interest:   " .$interest;
+echo "<br>";
+echo "Description:   " .$description;
+
+
+?>
